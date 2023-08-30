abfc5fe11cffe97c02bc71edaa3d39a5a924053a
prasanthi2023 <prasanthichaluvadi03@gmail.com>
2023-08-30 13:47:52 +0530

    throw keyword is added

---
+++ ExceptionDemo2.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void divideByZero() {
+
+        // throw an exception
+        throw new ArithmeticException("Trying to divide by 0");
+    }
+
+    public static void main(String[] args) {
+        divideByZero();
+    }
+}
+
