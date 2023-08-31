501ea5a2a7d692c1c06a031c4d38011a48d1b52b
prasanthi2023 <prasanthichaluvadi03@gmail.com>
2023-08-31 11:35:17 +0530

    null pointer exception file is added

---
+++ exeception/Gfg.java
@@ -0,0 +1,20 @@
+package exeception;
+
+class Gfg {
+    public static void main(String[] args) {
+        // Initializing String variable with null value
+        String ptr = null;
+
+        // Checking if ptr.equals null or works fine.
+        try {
+            // This line of code throws NullPointerException
+            // because ptr is null
+            if (ptr.equals("gfg"))
+                System.out.print("Same");
+            else
+                System.out.print("Not Same");
+        } catch (NullPointerException e) {
+            System.out.print("NullPointerException Caught");
+        }
+    }
+}
