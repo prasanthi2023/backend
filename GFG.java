520446cc96dd1cacddce8d00cbbcbe6c4f2498a0
prasanthi2023 <prasanthichaluvadi03@gmail.com>
2023-09-13 18:04:58 +0530

    collection of array list example is added

---
+++ GFG.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+class Test {
+    synchronized void test_function(int n)
+    {
+        // synchronized method
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(n + i);
+            try {
+                Thread.sleep(500);
+            }
+            catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
+
+// Driver Class
+public class GFG {
+    // Main function
+    public static void main(String args[])
+    {
+        // only one object
+        final Test obj = new Test();
+
+        Thread a = new Thread() {
+            public void run() { obj.test_function(15); }
+        };
+
+        Thread b = new Thread() {
+            public void run() { obj.test_function(30); }
+        };
+
+        a.start();
+        b.start();
+    }
+}
---
+++ TodoAdd.java
@@ -0,0 +1,14 @@
+import java.util.Collection;
+import java.util.ArrayList;
+
+public class TodoAdd {
+    public static void main(String args[]){
+        Collection <String> names=new ArrayList<> ();
+        names.add("vicky");
+        names.add("nicky");
+        names.add("lekhana");
+        System.out.println("List of names:" +names);
+
+
+    }
+}
